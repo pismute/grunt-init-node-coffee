44f7e93dee242d7fc2fad5d464fbd05870ae22ac
Changwoo Park <pismute@gmail.com>
2013-02-21 20:35:52 +0900

    Initial Commmit based on node scaffolding.

---
+++ template.js
@@ -0,0 +1,78 @@
+/*
+ * grunt-init-node
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a Node.js module in Coffeescript, including Mocha unit tests using Should assertion.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
+  'be a unique ID not already in use at search.npmjs.org.';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'You should now install project dependencies with _npm ' +
+  'install_. After that, you may execute project tasks with _grunt_. For ' +
+  'more information about installing and configuring Grunt, please see ' +
+  'the Getting Started guide:' +
+  '\n\n' +
+  'http://gruntjs.com/getting-started';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({type: 'node-coffee'}, [
+    // Prompt for these values.
+    init.prompt('name'),
+    init.prompt('description'),
+    init.prompt('version'),
+    init.prompt('repository'),
+    init.prompt('homepage'),
+    init.prompt('bugs'),
+    init.prompt('licenses'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('author_url'),
+    init.prompt('node_version'),
+    init.prompt('main', function(value, data, done) {
+      done(null, 'out/lib/' + data.name);
+    }),
+    init.prompt('npm_test', 'grunt simplemocha')
+  ], function(err, props) {
+    props.keywords = [];
+    props.devDependencies = {
+      'grunt-contrib-jshint': '~0.1.1',
+      'grunt-contrib-watch': '~0.2.0',
+      'grunt-contrib-coffee': '~0.4.0',
+      'grunt-contrib-clean': '~0.4.0',
+      'grunt-contrib-copy': '~0.4.0',
+      'grunt-coffeelint': '~0.0.5',
+      'grunt-simple-mocha': '~0.3.2',
+      'should': '~1.2.2',
+    };
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Add properly-named license files.
+    init.addLicenseFiles(files, props.licenses);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // Generate package.json file.
+    init.writePackageJSON('package.json', props);
+
+    // All done!
+    done();
+  });
+
+};
