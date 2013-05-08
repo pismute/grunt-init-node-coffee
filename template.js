3b89fa1fe8e83848b31ebd9336ec8812fb06462e
Nicholas Calugar <njcalugar@gmail.com>
2013-05-08 16:27:52 -0700

    Updating deps to latest versions

--- template.js
+++ template.js
@@ -49,13 +49,13 @@ exports.template = function(grunt, init, done) {
   ], function(err, props) {
     props.keywords = [];
     props.devDependencies = {
-      'grunt-contrib-jshint': '~0.1.1',
-      'grunt-contrib-watch': '~0.2.0',
-      'grunt-contrib-coffee': '~0.4.0',
-      'grunt-contrib-clean': '~0.4.0',
-      'grunt-contrib-copy': '~0.4.0',
-      'grunt-coffeelint': '~0.0.5',
-      'grunt-simple-mocha': '~0.3.2',
+      'grunt-contrib-jshint': '~0.4.3',
+      'grunt-contrib-watch': '~0.4.0',
+      'grunt-contrib-coffee': '~0.7.0',
+      'grunt-contrib-clean': '~0.4.1',
+      'grunt-contrib-copy': '~0.4.1',
+      'grunt-coffeelint': '~0.0.6',
+      'grunt-simple-mocha': '~0.4.0',
       'should': '~1.2.2',
     };
 
